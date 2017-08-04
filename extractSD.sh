29e28a82a0ed5fbc9b7669f6ed32746e0a4c7e2e
Jon C <goeland86@gmail.com>
2017-08-04 09:55:36 +0200

    Update extractSD.sh

--- extractSD.sh
+++ extractSD.sh
@@ -5,6 +5,9 @@ sudo true
 DISK=/dev/mmcblk0
 PART="$DISK""p1"
 umount $PART
-COUNT=`sudo parted /dev/mmcblk0 print | grep boot | awk '{print $3}' | awk -F'M' '{print $1}'`
+
 IMG=$1
+
+COUNT=`sudo parted /dev/mmcblk0 print | grep boot | awk '{print $3}' | awk -F'M' '{print $1}'`
+
 sudo dd if=$DISK bs=1MB count=$COUNT | pv | xz -c | dd of=$IMG
